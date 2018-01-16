9b2329c575a802cb0a8ab8c757f9ecc1c1bf6d3a
dskrad <dskrad@me.com>
2018-01-16 16:42:14 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
+    version='0.1.2',
     description='tool for inserting chapter marks in mp3 files',
     long_description=open("README.rst", encoding="utf8").read(),
 
