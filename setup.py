d2ce562ad850d18a26412426e1b11583bbff6e9b
dskrad <dskrad@me.com>
2018-01-17 15:34:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,48 +1,109 @@
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'mp3chaps'
+VERSION = '0.1.2'
+KEYWORDS = 'mp3 chapters'
+DESCRIPTION = 'tool for inserting chapter marks in mp3 files'
+URL = 'https://github.com/dskrad/mp3chaps'
+EMAIL = 'dskcl1@gmail.com'
+AUTHOR = 'David Karimeddini'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'docopt>=0.6.2', 'eyeD3>=0.8.4'
+    ]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+  long_description = '\n' + f.read()
+
+
+class UploadCommand(Command):
+  """Support setup.py upload."""
+
+  description = 'Build and publish the package.'
+  user_options = []
+
+  @staticmethod
+  def status(s):
+    """Prints things in bold."""
+    print('\033[1m{0}\033[0m'.format(s))
+
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    pass
+
+  def run(self):
+    try:
+      self.status('Removing previous builds…')
+      rmtree(os.path.join(here, 'dist'))
+    except OSError:
+      pass
+
+    self.status('Building Source and Wheel (universal) distribution…')
+    os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+    self.status('Uploading the package to PyPi via Twine…')
+    os.system('twine upload dist/*')
+
+    sys.exit()
+
 
 setup(
-    name='mp3chaps',
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.2',
-    description='tool for inserting chapter marks in mp3 files',
-    long_description=open("README.rst", encoding="utf8").read(),
+  name=NAME,
+  version=VERSION,
+  description=DESCRIPTION,
+  long_description=long_description,
+  author=AUTHOR,
+  author_email=EMAIL,
+  url=URL,
+  #packages=find_packages(exclude=('tests',)),
+  # If your package is a single module, use this instead of 'packages':
+  py_modules=["mp3chaps"],
 
-    url='https://github.com/dskrad/mp3chaps',
+  entry_points={
+    'console_scripts': [ 'mp3chaps=mp3chaps:main' ],
+    },
+  install_requires=REQUIRED,
+  include_package_data=True,
+  license='MIT',
+  keywords=KEYWORDS,
 
-    author='David Karimeddini',
-    author_email='dskcl1@gmail.com',
-    license='MIT',
-
-    keywords='mp3 chapters',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    #packages=find_packages(),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    py_modules=["mp3chaps"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-      'docopt>=0.6.2',
-      'eyeD3>=0.8.4'
-      ],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'mp3chaps=mp3chaps:main',
-        ],
+  classifiers=[
+    # Trove classifiers
+    # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    #'Programming Language :: Python :: 3',
+    #'Programming Language :: Python :: 3.3',
+    #'Programming Language :: Python :: 3.4',
+    #'Programming Language :: Python :: 3.5',
+    #'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+  # $ setup.py publish support.
+  cmdclass={
+    'upload': UploadCommand
     },
 )
